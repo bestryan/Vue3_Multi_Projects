e39e20f149046fb656eb9ea3a243c93ab74b2ce4
Ryan Xiao <77907899+bestryan@users.noreply.github.com>
2023-09-26 12:53:03 +1000

    first commit
    reaction timer

---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction time: - {{ sc }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ['sc'],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.sc < 250) {
+      this.rank = 'Ninja Fingers';
+    } else if (this.sc < 400) {
+      this.rank = 'Rapid Reflexes';
+    } else {
+      this.rank = 'sanil pace...';
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
